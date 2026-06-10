39d76b96be85030b68970dc459f1f87cbe370c48
Kaaya77 <101862137+Kaaya77@users.noreply.github.com>
2026-06-10 22:24:28 +0300

    update2

--- App.tsx
+++ App.tsx
@@ -259,6 +259,7 @@ const AppContent = () => {
               <Route path="/auth/reset" element={<ResetPasswordPage />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
+            </Suspense>
           </motion.div>
         </AnimatePresence>
       </main>
